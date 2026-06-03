f86014fd121b3079ea782a14c5cb334875d6e014
akshat <akshatraj@gmail.com>
2026-06-03 14:59:21 +0530

    main function and functions

--- seventh.py
+++ seventh.py
@@ -54,10 +54,84 @@
     
 #     print(rev)
 
+# ReverseNo()
+
 
 # ReverseNo()
 
-def Sum(a,b):
-    return a+b
+# def Add(a,b):
+#     return a+b
 
-print(Sum(5,8))
+# def Sub(a,b):
+#     return a-b
+
+# def mul(a,b):
+#     return a*b
+
+# def div(a,b):
+#     if(b==0):
+#         print('infinty')
+#     else:
+#         return a/b
+    
+
+# def Clac():
+#     num1=int(input('enter the first number '))
+#     num2=int(input('enter the second number '))
+#     op=input('enter operator ')
+
+#     if(op=='+'):
+#         print(Add(num1,num2))
+#     elif(op=='-'):
+#         print(Sub(num1,num2))
+#     elif(op=='*'):
+#         print(mul(num1,num2))
+#     elif(op=='/'):
+#         print(div(num1,num2))
+
+# Clac()
+
+
+
+# def add(a,b):
+#         return a+b
+    
+# def sub(a,b):
+#         return a-b
+    
+# def mul(a,b):
+#         return a*b
+    
+# def div(a,b):
+#         if(b==0):
+#             print('infinty')
+#         else:
+#             return a/b
+
+# def main():
+#     a=float(input('enter a number '))
+#     b=float(input('enter a number '))
+#     op=input('enter operation ')
+
+#     if(op=='+'):
+#         print(add(a,b))
+#     elif(op=='-'):
+#         print(sub(a,b))
+#     elif(op=='*'):
+#         print(mul(a,b))
+#     elif(op=='/'):
+#         print(div(a,b))
+#     else:
+#         print('enter a valid number or operator')
+
+# main()
+
+
+# for x in range(1,11):
+#     if(x%2==0):
+#         print('even',x)
+
+
+for x in range(1,11):
+    if(x%2!=0):
+        print('odd',x)
