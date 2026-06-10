1a23ce39e74aaf1870e46c619ebb708fdd866a40
akshat <akshatraj@gmail.com>
2026-06-10 14:51:48 +0530

    arrays i,e list

--- second.py
+++ second.py
@@ -28,9 +28,52 @@
 # print(name[:5])         #prints string from the start(right) to the desired end, in this case [0:5] 
 
 
-name='akshat'
+# name='akshat'
 
+# user='soilder boy'
 i=0
-while(i<=len(name)):
-    print(name[-:])
-    i=i+1
+# cnt=0
+# # while(i<=len(user)):
+# #     print(user[i])
+# #     i=i+1
+# while(i<=len(user)):
+#     if(user[i]==' '):
+#         print('space detected',cnt+1)
+    # i=i+1
+
+# rev=''
+# i=len(user)-1
+# while(i>=0):
+#     rev=rev+user[i]
+#     i=i-1
+
+# print(rev)
+
+# rev=''
+# for x in user:
+#     rev=x+rev
+
+# print(rev)
+
+
+# if user namer is palindrome or not 
+# user=input('enter your name ')
+# rev=''
+
+# for x in user:
+#     rev=x+rev
+
+
+# if(rev==user):
+#     print('username is palindrome ')
+# else:
+#     print('username is not palindrome')
+
+
+# string is a data structure particularly known as group of charcters 
+# string is immutable
+
+# user='soilder-boy'
+# print(user)               #string is immutable here when user is printed
+# print(user.capitalize())     # change to string was done in this line only
+
---
+++ third.py
@@ -0,0 +1,19 @@
+# arrays is a datastructure that stores diffrent type of elements
+# its known as list[] in python
+
+
+num=[1,2,3,4,56,7,89,9,]
+# print(num[0])
+# print(len(num)//2)         #use len(obj)//2  for dynamic lists
+# print(num[-1])
+# print(len(num))
+
+
+
+# i=0
+# while(i<=len(num)):
+#     print(num[i])
+#     i=i+1
+
+for x in num:
+    print(x)
